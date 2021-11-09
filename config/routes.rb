88f0cd3706e6ea1cf6041cba61feba39c6034896
noemie basile <noemiebasile92@gmail.com>
2021-11-09 15:13:51 +0100

    exo done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index'
+  get "tasks/new", to: "tasks#new", as: :new
+  post "tasks", to: "tasks#create"
+
+  get 'tasks', to: 'tasks#index', as: :index
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+
+  patch  "tasks/:id",      to: "tasks#update"
+  delete "tasks/:id",      to: "tasks#destroy"
 
 end
